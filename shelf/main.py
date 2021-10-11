3a265a5c770f5cc2aa4cd8c3dbd589ebe90b7a39
lvittor <lvittor@itba.edu.ar>
2021-10-11 20:20:49 -0300

    chore: develop console poc
    Signed-off-by: lvittor <lvittor@itba.edu.ar>

--- shelf/main.py
+++
@@ -1,6 +0,0 @@
-def foo(n):
-    return n
-
-
-if __name__ == "__main__":
-    foo(2)
---
+++ shelf/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="shelf",
+    version="1.0",
+    py_modules=["shelf"],
+    install_requires=[
+        "Click",
+    ],
+    entry_points="""
+        [console_scripts]
+        shelf=shelf:cli
+    """,
+)
